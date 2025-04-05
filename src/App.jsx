74a38e4b0351955a6cda11d70a5cc4b1709bf127
abhishekraj <iamraj98985@gmail.com>
2025-04-06 02:14:56 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Button, Container, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <Container maxWidth="sm" style={{ marginTop: '2rem' }}>
+      <Typography variant="h4" gutterBottom>
+        Welcome to FinFlow 💰
+      </Typography>
+      <Button variant="contained" color="primary">
+        Track Your Expenses
+      </Button>
+    </Container>
+  );
+}
+
+export default App;
