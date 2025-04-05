3aa7ade89ec4161c42a2c8c64f7d85dec63955a1
abhishekraj <iamraj98985@gmail.com>
2025-04-06 03:08:12 +0530

    added view all users button on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Users from './pages/Users';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>Home Page</div>} />
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1>Home Page</h1>
+              <Link to="/users">
+                <button>View All Users</button>
+              </Link>
+            </div>
+          }
+        />
         <Route path="/users" element={<Users />} />
       </Routes>
     </Router>
