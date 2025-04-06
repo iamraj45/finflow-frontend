46b23365526460b1f9d25b4806ed234d4ad45abe
abhishekraj <iamraj98985@gmail.com>
2025-04-06 15:52:54 +0530

    fixed homepage error

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 import AddExpenseForm from "../components/AddExpenseForm";
 
-export default function HomePage() {
+export default function Homepage() {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">FinFlow</h1>
+      <h1 className="text-2xl font-bold mb-4">FinFlow - Your personal expense tracker</h1>
       <Link to="/users">
         <button className="mb-6 bg-gray-300 px-4 py-2 rounded hover:bg-gray-400">
-          View All Users
+          View Users
         </button>
       </Link>
 
