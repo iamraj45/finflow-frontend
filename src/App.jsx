f59e7270abd85a40660b250268ab78defb4dd49d
abhishekraj <iamraj98985@gmail.com>
2025-04-06 16:05:54 +0530

    fixed homepage issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Users from './pages/Users';
-import Homepage from './pages/Homepage';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<Home />} />
         <Route path="/users" element={<Users />} />
       </Routes>
     </Router>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import AddExpenseForm from "../components/AddExpenseForm";
 
-export default function Homepage() {
+export default function Home() {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4">FinFlow - Your personal expense tracker</h1>
